cbee41597fb3f436b9acc566a0dd745be587846b
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2026-01-06 20:45:56 +0530

    Initial setup: load dataset and verify enviroment

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Load the Netflix titles dataset
+df = pd.read_csv('data/netflix_titles.csv')
+
+# Basic Checks
+print("Shape:", df.shape)
+print("\nColumns:")
+print(df.columns)
+
+print("\nFirst 5 rows:")
+print(df.head())
